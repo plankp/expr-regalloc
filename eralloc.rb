e19e3fe5ff7aed3e4d8df78a76fbb6a38df113e3
Paul Teng <plankp@outlook.com>
2021-11-02 18:17:10 -0400

    Implement division and remainder

--- eralloc.rb
+++ eralloc.rb
@@ -6,7 +6,7 @@ def eralloc(expr)
     map = {}
     ershov(expr, map)
 
-    registers = ['edx', 'eax']
+    registers = ['edx', 'ecx', 'eax']
     emit(expr, map, Array.new(registers), Array.new(registers), 4)
 end
 
@@ -35,27 +35,121 @@ def emit(expr, map, free_regs, all_regs, base_offset)
 
         lhs_expr = expr[1]
         rhs_expr = expr[2]
+
         if lhs_num < rhs_num then
             # swap the left and right expressions and the top two registers
             lhs_expr, rhs_expr = rhs_expr, lhs_expr
             free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
         end
-        
-        lc = emit(lhs_expr, map, free_regs, all_regs, base_offset)
-        rc = emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
-        
+
+        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
+        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
+
         if lhs_num < rhs_num then
             # unswap the top two registers
             free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
         end
 
-        opc = { "Add" => "add", "Sub" => "sub", "Mul" => "imul" }[expr[0]]
-        body += lc + rc + ["#{opc} #{free_regs[-1]}, #{free_regs[-2]}"]
+        case expr[0]
+        when "Add"
+            body += ["add #{free_regs[-1]}, #{free_regs[-2]}"]
+        when "Sub"
+            body += ["sub #{free_regs[-1]}, #{free_regs[-2]}"]
+        when "Mul"
+            body += ["imul #{free_regs[-1]}, #{free_regs[-2]}"]
+        end
 
         if need_spilling then
             free_regs.shift(spills.length)
-            all_regs.each do |r|
-                body << "mov #{r}, [ebp-#{spills[r]}]" if spills[r]
+            spills.each do |r, offs|
+                body << "mov #{r}, [ebp-#{offs}]"
+            end
+        end
+
+        return body
+    when "Div", "Rem"
+        lhs_num = map[expr[1]]
+        rhs_num = map[expr[2]]
+
+        body = []
+        need_spilling = free_regs.length < 2
+        spills = {}
+        if need_spilling then
+            spilled_regs = all_regs - free_regs
+            free_regs.unshift(*spilled_regs)
+            spilled_regs.each do |r|
+                body << "mov [ebp-#{base_offset}], #{r}";
+                spills[r] = base_offset
+                base_offset += 4
+            end
+        end
+
+        lhs_expr = expr[1]
+        rhs_expr = expr[2]
+
+        if lhs_num < rhs_num then
+            # swap the left and right expressions and the top two registers
+            lhs_expr, rhs_expr = rhs_expr, lhs_expr
+            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+        end
+
+        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
+        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
+
+        if lhs_num < rhs_num then
+            # unswap the top two registers
+            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+        end
+
+        divident = free_regs[-1]
+        divisor = free_regs[-2]
+        idiv_out = expr[0] == "Div" ? "eax" : "edx"
+
+        # 32-bit division via idiv requires:
+        # * divident in eax
+        # * divisor not in edx
+        #
+        # the additional constraint is that the result must have the same
+        # register as whatever is in the divident.
+
+        # things that do not need emergency spilling:
+        # * the divident: we will store the result into it anyway
+        # * all free regs: they are not holding anything useful anyway
+        emergency_spill = ['eax', 'edx', 'ecx'] - [divident] - free_regs
+        emergency_spill.each do |r|
+            body << "mov [ebp-#{base_offset}], #{r}"
+            base_offset += 4
+        end
+
+        # our goal is to make eax the divident and ecx the divisor
+        if divisor == "eax"
+            if divident == "ecx" then
+                body << "xchg eax, ecx"
+            else
+                # assuming all else is correct, divident cannot be eax
+                # (because divisor is already eax...)
+                raise "ILLEGAL ALLOCATION" if divident == "eax"
+
+                body << "mov ecx, eax"
+                body << "mov eax, #{divident}"
+            end
+        else
+            body << "mov eax, #{divident}" unless "eax" == divident
+            body << "mov ecx, #{divisor}" unless "ecx" == divisor
+        end
+
+        body << "cdq"
+        body << "idiv ecx"
+        body << "mov #{divident}, #{idiv_out}" unless divident == idiv_out
+        emergency_spill.reverse_each do |r|
+            base_offset -= 4
+            body << "mov #{r}, [ebp-#{base_offset}]"
+        end
+
+        if need_spilling then
+            free_regs.shift(spills.length)
+            spills.each do |r, offs|
+                body << "mov #{r}, [ebp-#{offs}]"
             end
         end
 
@@ -67,7 +161,7 @@ def ershov(expr, map)
     case expr[0]
     when "Numeric", "Variable"
         return map[expr] = 1
-    when "Add", "Sub", "Mul"
+    when "Add", "Sub", "Mul", "Div", "Rem"
         lhs = ershov(expr[1], map)
         rhs = ershov(expr[2], map)
 
@@ -129,4 +223,24 @@ puts eralloc(["Sub",
     ["Add", ["Variable", "d"], ["Numeric", 3]],
     ["Add", ["Add", ["Variable", "b"], ["Variable", "c"]],
             ["Mul", ["Variable", "f"], ["Variable", "g"]]]])
+puts
+puts eralloc(["Div", ["Variable", "p"], ["Variable", "q"]])
+puts
+puts eralloc(["Div", ["Add", ["Numeric", 10], ["Variable", "p"]], ["Variable", "q"]])
+puts
+puts eralloc(["Div", ["Variable", "p"], ["Add", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Div",
+        ["Add", ["Numeric", 8], ["Variable", "p"]],
+        ["Add", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Div",
+        ["Div", ["Numeric", 8], ["Variable", "p"]],
+        ["Div", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Div",
+    ["Div", ["Div", ["Variable", "qq"], ["Variable", "rr"]],
+            ["Div", ["Variable", "ss"], ["Variable", "tt"]]],
+    ["Div", ["Div", ["Variable", "b"], ["Variable", "c"]],
+            ["Div", ["Variable", "f"], ["Variable", "g"]]]])
 puts
