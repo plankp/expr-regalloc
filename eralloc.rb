3ec77f127a55a692d91878935d27ecfa91cb7686
Paul Teng <plankp@outlook.com>
2021-11-02 18:55:06 -0400

    Implement shifts

--- eralloc.rb
+++ eralloc.rb
@@ -154,6 +154,76 @@ def emit(expr, map, free_regs, all_regs, base_offset)
         end
 
         return body
+    when "Shl", "Sra", "Srl"
+        lhs_num = map[expr[1]]
+        rhs_num = map[expr[2]]
+
+        body = []
+        need_spilling = free_regs.length < 2
+        spills = {}
+        if need_spilling then
+            spilled_regs = all_regs - free_regs
+            free_regs.unshift(*spilled_regs)
+            spilled_regs.each do |r|
+                body << "mov [ebp-#{base_offset}], #{r}";
+                spills[r] = base_offset
+                base_offset += 4
+            end
+        end
+
+        lhs_expr = expr[1]
+        rhs_expr = expr[2]
+
+        if lhs_num < rhs_num then
+            # swap the left and right expressions and the top two registers
+            lhs_expr, rhs_expr = rhs_expr, lhs_expr
+            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+        end
+
+        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
+        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
+
+        if lhs_num < rhs_num then
+            # unswap the top two registers
+            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+        end
+
+        shift_op = { "Shl" => "shl", "Sra" => "sar", "Srl" => "shr" }[expr[0]]
+        shifted = free_regs[-1]
+        shamt = free_regs[-2]
+
+        # shifting requires the shamt to be in ecx (specific cl, which is the
+        #  low 16 bits)
+        #
+        # the additional constraint is that the result must have the same
+        # register as whatever is in shifted.
+
+        if shamt == "ecx" then
+            body << "#{shift_op} #{shifted}, cl"
+        elsif shifted == "ecx" then
+            body << "xchg ecx, #{shamt}"
+            body << "#{shift_op} #{shamt}, cl"
+            body << "mov ecx, #{shamt}"
+        elsif free_regs.include?("ecx")
+            # we can just wipe ecx
+            body << "mov ecx, #{shamt}"
+            body << "#{shift_op} #{shifted}, cl"
+        else
+            # we need to save ecx, and since of the registers is free anyways,
+            # just use xchg twice and avoid the load-store.
+            body << "xchg ecx, #{shamt}"
+            body << "#{shift_op} #{shifted}, cl"
+            body << "xchg ecx, #{shamt}"
+        end
+
+        if need_spilling then
+            free_regs.shift(spills.length)
+            spills.each do |r, offs|
+                body << "mov #{r}, [ebp-#{offs}]"
+            end
+        end
+
+        return body
     end
 end
 
@@ -161,7 +231,7 @@ def ershov(expr, map)
     case expr[0]
     when "Numeric", "Variable"
         return map[expr] = 1
-    when "Add", "Sub", "Mul", "Div", "Rem"
+    when "Add", "Sub", "Mul", "Div", "Rem", "Shl", "Sra", "Srl"
         lhs = ershov(expr[1], map)
         rhs = ershov(expr[2], map)
 
@@ -243,4 +313,24 @@ puts eralloc(["Div",
             ["Div", ["Variable", "ss"], ["Variable", "tt"]]],
     ["Div", ["Div", ["Variable", "b"], ["Variable", "c"]],
             ["Div", ["Variable", "f"], ["Variable", "g"]]]])
+puts
+puts eralloc(["Shl", ["Variable", "x"], ["Numeric", 8]])
+puts
+puts eralloc(["Shl", ["Add", ["Numeric", 10], ["Variable", "p"]], ["Variable", "q"]])
+puts
+puts eralloc(["Shl", ["Variable", "p"], ["Add", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Shl",
+        ["Add", ["Numeric", 8], ["Variable", "p"]],
+        ["Add", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Shl",
+        ["Shl", ["Numeric", 8], ["Variable", "p"]],
+        ["Shl", ["Numeric", 10], ["Variable", "q"]]])
+puts
+puts eralloc(["Shl",
+    ["Shl", ["Shl", ["Variable", "qq"], ["Variable", "rr"]],
+            ["Shl", ["Variable", "ss"], ["Variable", "tt"]]],
+    ["Shl", ["Shl", ["Variable", "b"], ["Variable", "c"]],
+            ["Shl", ["Variable", "f"], ["Variable", "g"]]]])
 puts
