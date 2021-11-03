d96c7f3d68293ed146db2a2bb9aaebe626fc6f87
Paul Teng <plankp@outlook.com>
2021-11-03 06:06:50 -0400

    We be allocat'n

--- eralloc.rb
+++ eralloc.rb
@@ -2,274 +2,302 @@
 # Based on Ershov and Sethi-Ullman (which are pretty much the same anyway)
 #
 
-def eralloc(expr)
-    map = {}
-    ershov(expr, map)
+class ERAlloc
+    attr_reader :max_offset
 
-    registers = ['edx', 'ecx', 'eax']
-    emit(expr, map, Array.new(registers), Array.new(registers), 4)
-end
+    def alloc(expr)
+        @ershov_labels = {}
+        @registers = ['edx', 'ecx', 'eax']
+        @max_offset = 0
 
-def emit(expr, map, free_regs, all_regs, base_offset)
-    case expr[0]
-    when "Numeric"
-        return ["mov #{free_regs.last}, #{expr[1]}"]
-    when "ReadVar"
-        return ["mov #{free_regs.last}, [ebp-#{expr[1]}]"]
-    when "RefVar"
-        return ["lea #{free_regs.last}, [ebp-#{expr[1]}]"]
-    when "Add", "Sub", "Mul"
-        lhs_num = map[expr[1]]
-        rhs_num = map[expr[2]]
+        self.ershov(expr)
+        self.emit(expr, Array.new(@registers), 4)
+    end
 
-        body = []
-        need_spilling = free_regs.length < 2
-        spills = {}
-        if need_spilling then
-            spilled_regs = all_regs - free_regs
-            free_regs.unshift(*spilled_regs)
-            spilled_regs.each do |r|
-                body << "mov [ebp-#{base_offset}], #{r}";
-                spills[r] = base_offset
-                base_offset += 4
-            end
-        end
+    def update_offset(offset)
+        @max_offset = offset if offset > @max_offset
+        offset
+    end
 
-        lhs_expr = expr[1]
-        rhs_expr = expr[2]
-
-        if lhs_num < rhs_num then
-            # swap the left and right expressions and the top two registers
-            lhs_expr, rhs_expr = rhs_expr, lhs_expr
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
-
-        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
-        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
-
-        if lhs_num < rhs_num then
-            # unswap the top two registers
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
-
+    def ershov(expr)
         case expr[0]
-        when "Add"
-            body += ["add #{free_regs[-1]}, #{free_regs[-2]}"]
-        when "Sub"
-            body += ["sub #{free_regs[-1]}, #{free_regs[-2]}"]
-        when "Mul"
-            body += ["imul #{free_regs[-1]}, #{free_regs[-2]}"]
-        end
+        when "Numeric", "ReadVar", "RefVar"
+            return @ershov_labels[expr] = 1
+        when "Add", "Sub", "Mul", "Div", "Rem", "Shl", "Sra", "Srl"
+            lhs = self.ershov(expr[1])
+            rhs = self.ershov(expr[2])
 
-        if need_spilling then
-            free_regs.shift(spills.length)
-            spills.each do |r, offs|
-                body << "mov #{r}, [ebp-#{offs}]"
+            return @ershov_labels[expr] = lhs == rhs ? lhs + 1 : [lhs, rhs].max
+        when "Call"
+            # function calls are special because the amount of register (and what
+            # registers they use) are dictated by the calling convention.
+
+            # Regardless of calling convention, we must compute the call site as
+            # well as the arguments to the call.
+            expr[2..].each {|arg| self.ershov(arg)}
+            return @ershov_labels[expr] = self.ershov(expr[1])
+        end
+    end
+
+    def emit(expr, free_regs, base_offset)
+        case expr[0]
+        when "Numeric"
+            return ["mov #{free_regs.last}, #{expr[1]}"]
+        when "ReadVar"
+            return ["mov #{free_regs.last}, [ebp-#{expr[1]}]"]
+        when "RefVar"
+            return ["lea #{free_regs.last}, [ebp-#{expr[1]}]"]
+        when "Add", "Sub", "Mul"
+            lhs_num = @ershov_labels[expr[1]]
+            rhs_num = @ershov_labels[expr[2]]
+
+            body = []
+            need_spilling = free_regs.length < 2
+            spills = {}
+            if need_spilling then
+                spilled_regs = @registers - free_regs
+                free_regs.unshift(*spilled_regs)
+                spilled_regs.each do |r|
+                    body << "mov [ebp-#{base_offset}], #{r}";
+                    spills[r] = base_offset
+                    self.update_offset(base_offset += 4)
+                end
             end
-        end
 
-        return body
-    when "Div", "Rem"
-        lhs_num = map[expr[1]]
-        rhs_num = map[expr[2]]
+            lhs_expr = expr[1]
+            rhs_expr = expr[2]
 
-        body = []
-        need_spilling = free_regs.length < 2
-        spills = {}
-        if need_spilling then
-            spilled_regs = all_regs - free_regs
-            free_regs.unshift(*spilled_regs)
-            spilled_regs.each do |r|
-                body << "mov [ebp-#{base_offset}], #{r}";
-                spills[r] = base_offset
-                base_offset += 4
+            if lhs_num < rhs_num then
+                # swap the left and right expressions and the top two registers
+                lhs_expr, rhs_expr = rhs_expr, lhs_expr
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
             end
-        end
 
-        lhs_expr = expr[1]
-        rhs_expr = expr[2]
+            body += self.emit(lhs_expr, free_regs, base_offset)
+            body += self.emit(rhs_expr, free_regs[..-2], base_offset)
 
-        if lhs_num < rhs_num then
-            # swap the left and right expressions and the top two registers
-            lhs_expr, rhs_expr = rhs_expr, lhs_expr
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
+            if lhs_num < rhs_num then
+                # unswap the top two registers
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+            end
 
-        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
-        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
+            case expr[0]
+            when "Add"
+                body += ["add #{free_regs[-1]}, #{free_regs[-2]}"]
+            when "Sub"
+                body += ["sub #{free_regs[-1]}, #{free_regs[-2]}"]
+            when "Mul"
+                body += ["imul #{free_regs[-1]}, #{free_regs[-2]}"]
+            end
 
-        if lhs_num < rhs_num then
-            # unswap the top two registers
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
+            if need_spilling then
+                free_regs.shift(spills.length)
+                spills.each do |r, offs|
+                    body << "mov #{r}, [ebp-#{offs}]"
+                end
+            end
 
-        divident = free_regs[-1]
-        divisor = free_regs[-2]
-        idiv_out = expr[0] == "Div" ? "eax" : "edx"
+            return body
+        when "Div", "Rem"
+            lhs_num = @ershov_labels[expr[1]]
+            rhs_num = @ershov_labels[expr[2]]
 
-        # 32-bit division via idiv requires:
-        # * divident in eax
-        # * divisor not in edx
-        #
-        # the additional constraint is that the result must have the same
-        # register as whatever is in the divident.
+            body = []
+            need_spilling = free_regs.length < 2
+            spills = {}
+            if need_spilling then
+                spilled_regs = @registers - free_regs
+                free_regs.unshift(*spilled_regs)
+                spilled_regs.each do |r|
+                    body << "mov [ebp-#{base_offset}], #{r}";
+                    spills[r] = base_offset
+                    self.update_offset(base_offset += 4)
+                end
+            end
 
-        # things that do not need emergency spilling:
-        # * the divident: we will store the result into it anyway
-        # * all free regs: they are not holding anything useful anyway
-        emergency_spill = ['eax', 'edx', 'ecx'] - [divident] - free_regs
-        emergency_spill.each do |r|
-            body << "mov [ebp-#{base_offset}], #{r}"
-            base_offset += 4
-        end
+            lhs_expr = expr[1]
+            rhs_expr = expr[2]
 
-        # our goal is to make eax the divident and ecx the divisor
-        if divisor == "eax"
-            if divident == "ecx" then
-                body << "xchg eax, ecx"
+            if lhs_num < rhs_num then
+                # swap the left and right expressions and the top two registers
+                lhs_expr, rhs_expr = rhs_expr, lhs_expr
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+            end
+
+            body += emit(lhs_expr, free_regs, base_offset)
+            body += emit(rhs_expr, free_regs[..-2], base_offset)
+
+            if lhs_num < rhs_num then
+                # unswap the top two registers
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+            end
+
+            divident = free_regs[-1]
+            divisor = free_regs[-2]
+            idiv_out = expr[0] == "Div" ? "eax" : "edx"
+
+            # 32-bit division via idiv requires:
+            # * divident in eax
+            # * divisor not in edx
+            #
+            # the additional constraint is that the result must have the same
+            # register as whatever is in the divident.
+
+            # things that do not need emergency spilling:
+            # * the divident: we will store the result into it anyway
+            # * all free regs: they are not holding anything useful anyway
+            emergency_spill = ['eax', 'edx', 'ecx'] - [divident] - free_regs
+            emergency_spill.each do |r|
+                body << "mov [ebp-#{base_offset}], #{r}"
+                self.update_offset(base_offset += 4)
+            end
+
+            # our goal is to make eax the divident and ecx the divisor
+            if divisor == "eax"
+                if divident == "ecx" then
+                    body << "xchg eax, ecx"
+                else
+                    # assuming all else is correct, divident cannot be eax
+                    # (because divisor is already eax...)
+                    raise "ILLEGAL ALLOCATION" if divident == "eax"
+
+                    body << "mov ecx, eax"
+                    body << "mov eax, #{divident}"
+                end
             else
-                # assuming all else is correct, divident cannot be eax
-                # (because divisor is already eax...)
-                raise "ILLEGAL ALLOCATION" if divident == "eax"
-
-                body << "mov ecx, eax"
-                body << "mov eax, #{divident}"
+                body << "mov eax, #{divident}" unless "eax" == divident
+                body << "mov ecx, #{divisor}" unless "ecx" == divisor
             end
-        else
-            body << "mov eax, #{divident}" unless "eax" == divident
-            body << "mov ecx, #{divisor}" unless "ecx" == divisor
-        end
 
-        body << "cdq"
-        body << "idiv ecx"
-        body << "mov #{divident}, #{idiv_out}" unless divident == idiv_out
-        emergency_spill.reverse_each do |r|
-            base_offset -= 4
-            body << "mov #{r}, [ebp-#{base_offset}]"
-        end
-
-        if need_spilling then
-            free_regs.shift(spills.length)
-            spills.each do |r, offs|
-                body << "mov #{r}, [ebp-#{offs}]"
+            body << "cdq"
+            body << "idiv ecx"
+            body << "mov #{divident}, #{idiv_out}" unless divident == idiv_out
+            emergency_spill.reverse_each do |r|
+                base_offset -= 4
+                body << "mov #{r}, [ebp-#{base_offset}]"
             end
-        end
 
-        return body
-    when "Shl", "Sra", "Srl"
-        lhs_num = map[expr[1]]
-        rhs_num = map[expr[2]]
-
-        body = []
-        need_spilling = free_regs.length < 2
-        spills = {}
-        if need_spilling then
-            spilled_regs = all_regs - free_regs
-            free_regs.unshift(*spilled_regs)
-            spilled_regs.each do |r|
-                body << "mov [ebp-#{base_offset}], #{r}";
-                spills[r] = base_offset
-                base_offset += 4
+            if need_spilling then
+                free_regs.shift(spills.length)
+                spills.each do |r, offs|
+                    body << "mov #{r}, [ebp-#{offs}]"
+                end
             end
-        end
 
-        lhs_expr = expr[1]
-        rhs_expr = expr[2]
+            return body
+        when "Shl", "Sra", "Srl"
+            lhs_num = @ershov_labels[expr[1]]
+            rhs_num = @ershov_labels[expr[2]]
 
-        if lhs_num < rhs_num then
-            # swap the left and right expressions and the top two registers
-            lhs_expr, rhs_expr = rhs_expr, lhs_expr
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
-
-        body += emit(lhs_expr, map, free_regs, all_regs, base_offset)
-        body += emit(rhs_expr, map, free_regs[..-2], all_regs, base_offset)
-
-        if lhs_num < rhs_num then
-            # unswap the top two registers
-            free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
-        end
-
-        shift_op = { "Shl" => "shl", "Sra" => "sar", "Srl" => "shr" }[expr[0]]
-        shifted = free_regs[-1]
-        shamt = free_regs[-2]
-
-        # shifting requires the shamt to be in ecx (specific cl, which is the
-        # low 16 bits)
-        #
-        # the additional constraint is that the result must have the same
-        # register as whatever is in shifted.
-
-        if shamt == "ecx" then
-            body << "#{shift_op} #{shifted}, cl"
-        elsif shifted == "ecx" then
-            body << "xchg ecx, #{shamt}"
-            body << "#{shift_op} #{shamt}, cl"
-            body << "mov ecx, #{shamt}"
-        elsif free_regs.include?("ecx")
-            # we can just wipe ecx
-            body << "mov ecx, #{shamt}"
-            body << "#{shift_op} #{shifted}, cl"
-        else
-            # we need to save ecx, and since of the registers is free anyways,
-            # just use xchg twice and avoid the load-store.
-            body << "xchg ecx, #{shamt}"
-            body << "#{shift_op} #{shifted}, cl"
-            body << "xchg ecx, #{shamt}"
-        end
-
-        if need_spilling then
-            free_regs.shift(spills.length)
-            spills.each do |r, offs|
-                body << "mov #{r}, [ebp-#{offs}]"
+            body = []
+            need_spilling = free_regs.length < 2
+            spills = {}
+            if need_spilling then
+                spilled_regs = @registers - free_regs
+                free_regs.unshift(*spilled_regs)
+                spilled_regs.each do |r|
+                    body << "mov [ebp-#{base_offset}], #{r}";
+                    spills[r] = base_offset
+                    self.update_offset(base_offset += 4)
+                end
             end
+
+            lhs_expr = expr[1]
+            rhs_expr = expr[2]
+
+            if lhs_num < rhs_num then
+                # swap the left and right expressions and the top two registers
+                lhs_expr, rhs_expr = rhs_expr, lhs_expr
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+            end
+
+            body += emit(lhs_expr, free_regs, base_offset)
+            body += emit(rhs_expr, free_regs[..-2], base_offset)
+
+            if lhs_num < rhs_num then
+                # unswap the top two registers
+                free_regs[-1], free_regs[-2] = free_regs[-2], free_regs[-1]
+            end
+
+            shift_op = { "Shl" => "shl", "Sra" => "sar", "Srl" => "shr" }[expr[0]]
+            shifted = free_regs[-1]
+            shamt = free_regs[-2]
+
+            # shifting requires the shamt to be in ecx (specific cl, which is the
+            # low 16 bits)
+            #
+            # the additional constraint is that the result must have the same
+            # register as whatever is in shifted.
+
+            if shamt == "ecx" then
+                body << "#{shift_op} #{shifted}, cl"
+            elsif shifted == "ecx" then
+                body << "xchg ecx, #{shamt}"
+                body << "#{shift_op} #{shamt}, cl"
+                body << "mov ecx, #{shamt}"
+            elsif free_regs.include?("ecx")
+                # we can just wipe ecx
+                body << "mov ecx, #{shamt}"
+                body << "#{shift_op} #{shifted}, cl"
+            else
+                # we need to save ecx, and since of the registers is free anyways,
+                # just use xchg twice and avoid the load-store.
+                body << "xchg ecx, #{shamt}"
+                body << "#{shift_op} #{shifted}, cl"
+                body << "xchg ecx, #{shamt}"
+            end
+
+            if need_spilling then
+                free_regs.shift(spills.length)
+                spills.each do |r, offs|
+                    body << "mov #{r}, [ebp-#{offs}]"
+                end
+            end
+
+            return body
+        when "Call"
+            # the simplest way to do this is the following:
+            # 1. evaluate the arguments from last to first (also push them in that
+            #    order)
+            # 2. evaluate the callsite
+            # 3. issue the call instruction
+            # 4. move the result from eax
+            # 5. pop-off all the arguments
+
+            raise "OUT OF REGS" if free_regs.length < 1
+
+            body = []
+            expr[2..].reverse_each do |arg|
+                body += emit(arg, free_regs, base_offset)
+                body << "push #{free_regs.last}"
+            end
+
+            body += emit(expr[1], free_regs, base_offset)
+            body << "call eax"
+            body << "mov #{free_regs.last}, eax" unless free_regs.last == "eax"
+            body << "add esp, #{4 * (expr.length - 2)}" unless expr.length < 3
+
+            return body
         end
-
-        return body
-    when "Call"
-        # the simplest way to do this is the following:
-        # 1. evaluate the arguments from last to first (also push them in that
-        #    order)
-        # 2. evaluate the callsite
-        # 3. issue the call instruction
-        # 4. move the result from eax
-        # 5. pop-off all the arguments
-
-        raise "OUT OF REGS" if free_regs.length < 1
-
-        body = []
-        expr[2..].reverse_each do |arg|
-            body += emit(arg, map, free_regs, all_regs, base_offset)
-            body << "push #{free_regs.last}"
-        end
-
-        body += emit(expr[1], map, free_regs, all_regs, base_offset)
-        body << "call eax"
-        body << "mov #{free_regs.last}, eax" unless free_regs.last == "eax"
-        body << "add esp, #{4 * (expr.length - 2)}" unless expr.length < 3
-
-        return body
     end
 end
 
-def ershov(expr, map)
-    case expr[0]
-    when "Numeric", "ReadVar", "RefVar"
-        return map[expr] = 1
-    when "Add", "Sub", "Mul", "Div", "Rem", "Shl", "Sra", "Srl"
-        lhs = ershov(expr[1], map)
-        rhs = ershov(expr[2], map)
+def eralloc(expr)
+    v = ERAlloc.new
+    body = v.alloc(expr)
 
-        return map[expr] = lhs == rhs ? lhs + 1 : [lhs, rhs].max
-    when "Call"
-        # function calls are special because the amount of register (and what
-        # registers they use) are dictated by the calling convention.
+    prologue = []
+    prologue << "push ebp"
+    prologue << "mov ebp, esp"
+    prologue << "sub esp, #{v.max_offset}" if v.max_offset > 0
 
-        # Regardless of calling convention, we must compute the call site as
-        # well as the arguments to the call.
-        expr[2..].each {|arg| ershov(arg, map)}
-        return map[expr] = ershov(expr[1], map)
-    end
+    epilogue = [
+        "mov esp, ebp",
+        "pop ebp",
+        "ret"
+    ]
+
+    return prologue + body + epilogue
 end
 
 puts eralloc(["Numeric", 123])
