9fc28aa3ef71c69a5042bb6a6ee333994a0da44a
Paul Teng <plankp@outlook.com>
2021-11-09 18:06:48 -0500

    Make IRInstr immutable

--- app/src/main/java/com/ymcmp/eralloc/IRInstr.java
+++ app/src/main/java/com/ymcmp/eralloc/IRInstr.java
@@ -1,15 +1,18 @@
 package com.ymcmp.eralloc;
 
+import java.util.*;
+
 public final class IRInstr {
 
     public final String opcode;
-    public final String[] outputs;
-    public final String[] inputs;
+    public final List<String> outputs;
+    public final List<String> inputs;
 
     public IRInstr(String opcode, String[] outputs, String[] inputs) {
         this.opcode = opcode;
-        this.outputs = outputs;
-        this.inputs = inputs;
+
+        this.outputs = Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(outputs, outputs.length)));
+        this.inputs = Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(inputs, inputs.length)));
     }
 
     public static IRInstr makev(String opcode, String out, String... in) {
@@ -24,20 +27,22 @@ public final class IRInstr {
     public String toString() {
         final StringBuilder sb = new StringBuilder();
 
-        if (this.outputs.length > 0) {
-            sb.append(this.outputs[0]);
-            for (int i = 1; i < this.outputs.length; ++i)
-                sb.append(this.outputs[i]);
+        if (!this.outputs.isEmpty()) {
+            final Iterator<String> it = this.outputs.iterator();
+            sb.append(it.next());
+            while (it.hasNext())
+                sb.append(", ").append(it.next());
 
             sb.append(" = ");
         }
 
         sb.append(this.opcode);
 
-        if (this.inputs.length > 0) {
-            sb.append(' ').append(this.inputs[0]);
-            for (int i = 1; i < this.inputs.length; ++i)
-                sb.append(", ").append(this.inputs[i]);
+        if (!this.inputs.isEmpty()) {
+            final Iterator<String> it = this.inputs.iterator();
+            sb.append(' ').append(it.next());
+            while (it.hasNext())
+                sb.append(", ").append(it.next());
         }
 
         return sb.toString();
